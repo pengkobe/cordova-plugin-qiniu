ee0e3e6d33b7b93dace549b87c18ec873d4ae011
kobepeng <pengkobe123@gmail.com>
2018-02-09 14:57:50 +0800

    feat: the first version of android code

--- www/qiniu.js
+++ www/qiniu.js
@@ -1,12 +1,25 @@
-cordova.define('cordova/plugins/qiniu', function(
-  require,
-  exports,
-  module
-) {
+cordova.define('cordova/plugins/qiniu', function(require, exports, module) {
   var exec = require('cordova/exec');
 
   var Qiniu = function() {};
   var Qiniu = new Qiniu();
+  Qiniu.prototype.simpleUploadFile = function(args, successCallback) {
+    cordova.exec(successCallback, this.errorCallback, 'QiNiuPlugin', args);
+  };
+
+  Qiniu.prototype.init = function(uploadToken) {
+    cordova.exec(
+      successCallback,
+      this.errorCallback,
+      'QiNiuPlugin',
+      uploadToken
+    );
+  };
+
+  Qiniu.prototype.cancell = function(cancell) {
+    cordova.exec(successCallback, this.errorCallback, 'QiNiuPlugin', cancell);
+  };
+
   module.exports = Qiniu;
 });
 
