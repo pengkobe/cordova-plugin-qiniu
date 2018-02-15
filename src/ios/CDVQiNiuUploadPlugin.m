77124ca00012a88d01ab9eba2b11cd6513ad4b61
pengkobe <yipeng.info@gmail.com>
2018-02-15 21:20:57 +0800

    feat: add cancel method

--- src/ios/CDVQiNiuUploadPlugin.m
+++ src/ios/CDVQiNiuUploadPlugin.m
@@ -4,7 +4,7 @@
 
 @property (copy, nonatomic)NSString *token;
 @property (copy) NSString *callbackId;
-// @property (nonatomic) BOOL *flag;
+@property (nonatomic) BOOL flag;
 @property (copy) QNConfiguration *config;
 
 @end
@@ -36,18 +36,17 @@
 }
 
 
-//- (void)cancel:(CDVInvokedUrlCommand *)command{
-//    self.flag = true;
-//}
+- (void)cancel:(CDVInvokedUrlCommand *)command{
+    self.flag = TRUE;
+}
 
 
 // 简单文件上传
 -(void)simpleUploadFile:(CDVInvokedUrlCommand *)command
 {
     __block CDVPluginResult* pluginResult = nil;
-    NSMutableArray* result = [[NSMutableArray alloc] init];
+    NSMutableDictionary* result = [NSMutableDictionary dictionaryWithCapacity: 3];
 
-    // 重用uploadManager。一般地，只需要创建一个uploadManager对象
     NSDictionary* options = [command.arguments objectAtIndex:0];
     NSLog(@"token:%@",command.callbackId);
 
@@ -60,20 +59,19 @@
 
     // 上传进度更新
     QNUploadOption *uploadOption = [[QNUploadOption alloc] initWithMime:nil progressHandler:^(NSString *key, float percent) {
-        NSLog(@"percent == %.2f", percent);
-        key = key != nil ? key : @"";
-        [result addObject:key];
-        [result addObject:[NSNumber numberWithFloat:percent]];
-        // 回传进度
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary: result];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }
-                                                                 params:nil
-                                                               checkCrc:NO
-                                                     cancellationSignal:nil
-                                                  //   cancellationSignal:^() {
-       // return self.flag;
-  //  }
+            NSLog(@"percent == %.2f", percent);
+            key = key != nil ? key : @"";
+            [result setObject:key forKey:@"key"];
+            [result setObject:[NSNumber numberWithFloat:percent] forKey:@"percent"];
+            // 回传进度
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary: result];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+        params:nil
+        checkCrc:NO
+        cancellationSignal:^BOOL() {
+            return self.flag;
+        }
                                     ];
 
     // 上传管理器
@@ -82,12 +80,12 @@
         if(info.ok)
         {
             NSLog(@"请求成功");
-            key = key != nil ? key : @"";
-            info = info != nil ? info : @"";
-            resp = resp != nil ? resp : @"";
-            [result addObject:key ];
-            [result addObject:@""];
-            [result addObject:resp];
+            key = key != nil ? key : nil;
+            info = info != nil ? info : nil;
+            resp = resp != nil ? resp : nil;
+            [result setObject:key forKey:@"key"];
+            // [result setObject:info forKey:@"info"];
+            [result setObject:resp forKey:@"resp"];
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary: result];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 
@@ -95,15 +93,13 @@
         else{
             NSLog(@"失败");
             key = key != nil ? key : @"";
-            
-            info = info != nil ? info : @"";
-            resp = resp != nil ? resp : @"";
-            [result addObject:key ];
-            [result addObject:@""];
-            [result addObject:resp];
+            info = info != nil ? info : nil;
+            resp = resp != nil ? resp : nil;
+            [result setObject:key forKey:@"key"];
+            // [result setObject:info forKey:@"info"];
+            [result setObject:resp forKey:@"resp"];
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary: result];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-            //如果失败，这里可以把info信息上报自己的服务器，便于后面分析上传错误原因
         }
         NSLog(@"info ===== %@", info);
         NSLog(@"resp ===== %@", resp);
@@ -113,7 +109,6 @@
 
 
 // ================ 断点续传配置 ======================
-// //设置断点续传
 // NSError *error;
 // builder.recorder =  [QNFileRecorder fileRecorderWithFolder:@"保存目录" error:&error];
 // }];
